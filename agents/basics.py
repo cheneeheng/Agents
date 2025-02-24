f4abcca03e8ee718e5906e4fcffaa163493e4648
EeHeng Chen <eeheng.chen@gmail.com>
2025-02-24 22:06:33 +0000

    Add Gemini agent implementation with debugging configuration + Add pylintrc

--- agents/basics.py
+++ agents/basics.py
@@ -4,51 +4,178 @@ Basics of using langgraph.
 Based on: https://langchain-ai.github.io/langgraph/tutorials/introduction/
 """
 
-from typing import Annotated
+import json
+from typing import Annotated, Any, Callable, cast
 
+import dotenv
 from langchain_community.tools.tavily_search import TavilySearchResults
-from langchain_core.messages import BaseMessage
-from langchain_core.tools import BaseTool
+from langchain_core.messages import AIMessage, ToolMessage
+from langchain_core.runnables import RunnableConfig
+from langchain_core.tools import tool
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import StateGraph
 from langgraph.graph.message import add_messages
+from langgraph.graph.state import CompiledStateGraph
 from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.types import Command, interrupt
 from typing_extensions import TypedDict
 
-
-class State(TypedDict):  # pylint: disable=C0115
-    messages: Annotated[list, add_messages]
+dotenv.load_dotenv()
 
 
-graph_builder = StateGraph(State)
+class BasicToolNode:
+    """A node that runs the tools requested in the last AIMessage."""
+
+    def __init__(self, tools_: list) -> None:
+        self.tools_by_name = {tool.name: tool for tool in tools_}
+
+    def __call__(self, inputs: dict[str, Any]) -> dict[str, list[ToolMessage]]:
+        if messages := inputs.get("messages", []):
+            message = messages[-1]
+        else:
+            raise ValueError("No message found in input")
+        outputs = []
+        for tool_call in message.tool_calls:
+            tool_result = self.tools_by_name[tool_call["name"]].invoke(
+                tool_call["args"]
+            )
+            outputs.append(
+                ToolMessage(
+                    content=json.dumps(tool_result),
+                    name=tool_call["name"],
+                    tool_call_id=tool_call["id"],
+                )
+            )
+        return {"messages": outputs}
 
 
-tool: BaseTool = TavilySearchResults(max_results=2)
-tools = [tool]
-llm = ChatGoogleGenerativeAI(
-    model="gemini-1.5-pro",
-    temperature=0,
-    max_tokens=None,
-    timeout=None,
-    max_retries=2,
-)
-llm_with_tools = llm.bind_tools(tools)  # type: ignore
+@tool
+def human_assistance(query: str) -> str:
+    """Request assistance from a human."""
+    human_response = interrupt({"query": query})
+    return human_response["data"]
 
 
-def chatbot(state: State):  # pylint: disable=C0116
-    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+class GeminiAgent:
+    """Simple gemini agent that uses a chatbot and tools."""
+
+    class State(TypedDict):
+        messages: Annotated[list, add_messages]
+
+    def __init__(self):
+        self.tools = [TavilySearchResults(max_results=2), human_assistance]
+        self.llm = ChatGoogleGenerativeAI(
+            model="gemini-1.5-pro",
+            temperature=0,
+            max_tokens=None,
+            timeout=None,
+            max_retries=2,
+        )
+        self.llm_with_tools = self.llm.bind_tools(self.tools)
+        self.memory = MemorySaver()
+        self.graph = self._build_graph(self.chatbot, self.tools, self.memory)
+        # For memory
+        self.config = RunnableConfig(configurable={"thread_id": "1"})
+
+    @staticmethod
+    def _build_graph(
+        chatbot: Callable[[State], dict[str, list[AIMessage]]],
+        tools: list[Any],
+        memory: MemorySaver,
+    ) -> CompiledStateGraph:
+        """Build the state graph for the agent.
+
+        Args:
+            chatbot (Callable[[State], dict[str, list[AIMessage]]]):
+                Chatbot function.
+            tools (list[Any]): List of tools.
+            memory (MemorySaver): Memory saver.
+
+        Returns:
+            CompiledStateGraph: The compiled state graph.
+        """
+        graph_builder = StateGraph(GeminiAgent.State)
+        graph_builder.add_node("chatbot", chatbot)
+        graph_builder.add_node("tools", ToolNode(tools))
+        graph_builder.add_conditional_edges("chatbot", tools_condition)
+        # Any time a tool is called, we return to the chatbot to decide
+        # the next step
+        graph_builder.add_edge("tools", "chatbot")
+        graph_builder.set_entry_point("chatbot")
+        graph = graph_builder.compile(checkpointer=memory)
+        graph.get_graph().draw_mermaid_png(output_file_path="graph.png")
+        return graph
+
+    def chatbot(self, state: State) -> dict[str, list[AIMessage]]:
+        message = cast(AIMessage, self.llm_with_tools.invoke(state["messages"]))
+        # Because we will be interrupting during tool execution,
+        # we disable parallel tool calling to avoid repeating any
+        # tool invocations when we resume.
+        assert len(message.tool_calls) <= 1
+        return {"messages": [message]}
+
+    def get_snapshot(self):
+        snapshot = self.graph.get_state(self.config)
+        print("=" * 80)
+        print("Snapshot: ", snapshot)
+        print("Next Snapshot: ", snapshot.next)
+
+    def stream(self, input: str):  # pylint: disable=W0622
+        """Stream the input through the graph and print the output.
+
+        Args:
+            input (str): The input to the agent.
+        """
+        # for event in graph.stream(
+        #     {"messages": [{"role": "user", "content": input}]}
+        # ):
+        #     for value in event.values():
+        #         print("Assistant:", value["messages"][-1].content)
+        events = self.graph.stream(
+            {"messages": [{"role": "user", "content": input}]},
+            config=self.config,
+            stream_mode="values",
+        )
+        for event in events:
+
+            if len(event["messages"][-1].tool_calls) > 0:
+                human_response = (
+                    "We, the experts are here to help! We'd recommend you "
+                    "check out LangGraph to build your agent."
+                    " It's much more reliable and extensible than simple "
+                    "autonomous agents."
+                )
+                human_command: Command = Command(
+                    resume={"data": human_response}
+                )
+                resumed_events = self.graph.stream(
+                    human_command,
+                    config=self.config,
+                    stream_mode="values",
+                )
+                for resumed_event in resumed_events:
+                    resumed_event["messages"][-1].pretty_print()
+
+            else:
+                event["messages"][-1].pretty_print()
 
 
-graph_builder.add_node("chatbot", chatbot)
+if __name__ == "__main__":
 
-tool_node = ToolNode(tools=[tool])
-graph_builder.add_node("tools", tool_node)
+    agent = GeminiAgent()
 
-graph_builder.add_conditional_edges(
-    "chatbot",
-    tools_condition,
-)
-# Any time a tool is called, we return to the chatbot to decide the next step
-graph_builder.add_edge("tools", "chatbot")
-graph_builder.set_entry_point("chatbot")
-graph = graph_builder.compile()
+    while True:
+        try:
+            user_input = input("User: ")
+            if user_input.lower() in ["quit", "exit", "q"]:
+                print("Goodbye!")
+                break
+
+            agent.stream(user_input)
+        except:  # pylint: disable=W0702
+            # fallback if input() is not available
+            user_input = "What do you know about LangGraph?"
+            print("User: " + user_input)
+            agent.stream(user_input)
+            break
