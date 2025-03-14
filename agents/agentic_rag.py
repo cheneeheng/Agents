7b7a4f7e2a0b3045eea72442b49f4723fe92e14e
EeHeng Chen <eeheng.chen@gmail.com>
2025-03-14 22:34:41 +0000

    Add new agentic RAG implementation, update requirements, and modify VSCode settings

---
+++ agents/agentic_rag.py
@@ -0,0 +1,326 @@
+"""
+https://langchain-ai.github.io/langgraph/tutorials/rag/langgraph_agentic_rag/
+"""
+
+# import getpass
+# import os
+
+
+# def _set_env(key: str):
+#     if key not in os.environ:
+#         os.environ[key] = getpass.getpass(f"{key}:")
+
+
+# _set_env("OPENAI_API_KEY")
+
+import dotenv
+
+dotenv.load_dotenv()
+
+import pprint
+from typing import Annotated, Literal, Sequence
+
+from langchain import hub
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import Chroma
+from langchain_core.messages import BaseMessage, HumanMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.tools.retriever import create_retriever_tool
+from langchain_core.tools.simple import Tool
+from langchain_core.vectorstores import VectorStoreRetriever
+from langchain_google_genai import (
+    ChatGoogleGenerativeAI,
+    GoogleGenerativeAIEmbeddings,
+)
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langgraph.graph import END, START
+from langgraph.graph.message import add_messages
+from langgraph.graph.state import CompiledStateGraph, StateGraph
+from langgraph.prebuilt import ToolNode, tools_condition
+from pydantic import BaseModel, Field
+from typing_extensions import TypedDict
+
+
+def create_llm_model() -> ChatGoogleGenerativeAI:
+    """Create a ChatGoogleGenerativeAI model.
+
+    Returns:
+        ChatGoogleGenerativeAI: A ChatGoogleGenerativeAI model.
+    """
+    return ChatGoogleGenerativeAI(
+        # model="gemini-2.0-flash-lite",
+        model="gemini-2.0-flash",
+        temperature=0,
+        max_tokens=None,
+        timeout=None,
+        max_retries=2,
+    )
+
+
+# -----------------------------------------------------------------------------
+def create_retriever() -> VectorStoreRetriever:
+    """Create a retriever for the Lilian Weng blog posts on LLM agents,
+    prompt engineering, and adversarial attacks on LLMs.
+
+    Returns:
+        VectorStoreRetriever: A retriever for the Lilian Weng blog posts.
+    """
+    urls = [
+        "https://lilianweng.github.io/posts/2023-06-23-agent/",
+        "https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/",
+        "https://lilianweng.github.io/posts/2023-10-25-adv-attack-llm/",
+    ]
+    docs = [WebBaseLoader(url).load() for url in urls]
+    docs_list = [item for sublist in docs for item in sublist]
+    text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=100, chunk_overlap=50
+    )
+    doc_splits = text_splitter.split_documents(docs_list)
+    # Add to vectorDB
+    vectorstore = Chroma.from_documents(
+        documents=doc_splits,
+        collection_name="rag-chroma",
+        embedding=GoogleGenerativeAIEmbeddings(
+            model="models/text-embedding-004"
+        ),
+    )
+    return vectorstore.as_retriever()
+
+
+# -----------------------------------------------------------------------------
+def create_tools() -> list[Tool]:
+    """Create tools for the Lilian Weng blog posts on LLM agents,
+    prompt engineering, and adversarial attacks on LLMs.
+
+    Returns:
+        list: A list of tools for the Lilian Weng blog posts.
+    """
+    retriever = create_retriever()
+    retriever_tool = create_retriever_tool(
+        retriever,
+        "retrieve_blog_posts",
+        "Search and return information about Lilian Weng blog posts on LLM"
+        "agents, prompt engineering, and adversarial attacks on LLMs.",
+    )
+    return [retriever_tool]
+
+
+tools = create_tools()
+
+
+# -----------------------------------------------------------------------------
+class AgentState(TypedDict):
+    # The add_messages function defines how an update should be processed
+    # Default is to replace. add_messages says "append"
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+
+
+# -----------------------------------------------------------------------------
+# Edges
+
+
+def grade_documents(state: AgentState) -> Literal["generate", "rewrite"]:
+    """
+    Determines whether the retrieved documents are relevant to the question.
+
+    Args:
+        state (messages): The current state
+
+    Returns:
+        str: A decision for whether the documents are relevant or not
+    """
+    print("---CHECK RELEVANCE---")
+
+    messages = state["messages"]
+    last_message = messages[-1]
+    question = messages[0].content
+    docs = last_message.content
+
+    # Data model
+    class Grade(BaseModel):
+        """Binary score for relevance check."""
+
+        binary_score: str = Field(description="Relevance score 'yes' or 'no'")
+
+    # LLM
+    model = create_llm_model()
+    # LLM with tool and validation
+    llm_with_tool = model.with_structured_output(Grade)
+    # Prompt
+    # pylint: disable=C0301
+    prompt = PromptTemplate(
+        template="""You are a grader assessing relevance of a retrieved document to a user question. \n
+        Here is the retrieved document: \n\n {context} \n\n
+        Here is the user question: {question} \n
+        If the document contains keyword(s) or semantic meaning related to the user question, grade it as relevant. \n
+        Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question.""",
+        input_variables=["context", "question"],
+    )
+    # Chain
+    chain = prompt | llm_with_tool
+    # Run
+    scored_result = chain.invoke({"question": question, "context": docs})
+
+    score = (
+        scored_result.binary_score  # type: ignore[union-attr]  # pyright: ignore[reportAttributeAccessIssue]
+    )
+
+    if score == "yes":
+        print("---DECISION: DOCS RELEVANT---")
+        return "generate"
+
+    else:
+        print("---DECISION: DOCS NOT RELEVANT---")
+        print(score)
+        return "rewrite"
+
+
+# -----------------------------------------------------------------------------
+# Nodes
+
+
+def agent(state: AgentState) -> dict[str, list[BaseMessage]]:
+    """
+    Invokes the agent model to generate a response based on the current
+    state. Given the question, it will decide to retrieve using the
+    retriever tool, or simply end.
+
+    Args:
+        state (messages): The current state
+
+    Returns:
+        dict: The updated state with the agent response appended to messages
+    """
+    print("---CALL AGENT---")
+    messages = state["messages"]
+    model = create_llm_model()
+    model = model.bind_tools(  # type: ignore[union-attr, assignment]
+        tools  # type: ignore[arg-type]  # pyright: ignore[reportArgumentType]
+    )
+    response = model.invoke(messages)
+    # We return a list, because this will get added to the existing list
+    return {"messages": [response]}
+
+
+def rewrite(state) -> dict[str, list[BaseMessage]]:
+    """
+    Transform the query to produce a better question.
+
+    Args:
+        state (messages): The current state
+
+    Returns:
+        dict: The updated state with re-phrased question
+    """
+    print("---TRANSFORM QUERY---")
+    messages = state["messages"]
+    question = messages[0].content
+    msg = [
+        HumanMessage(
+            content=f""" \n
+    Look at the input and try to reason about the underlying semantic intent / meaning. \n
+    Here is the initial question:
+    \n ------- \n
+    {question}
+    \n ------- \n
+    Formulate an improved question: """,
+        )
+    ]
+    # Grader
+    model = create_llm_model()
+    response = model.invoke(msg)
+    return {"messages": [response]}
+
+
+def generate(state: AgentState) -> dict[str, list[BaseMessage]]:
+    """
+    Generate answer
+
+    Args:
+        state (messages): The current state
+
+    Returns:
+         dict: The updated state with re-phrased question
+    """
+    print("---GENERATE---")
+    messages = state["messages"]
+    question = messages[0].content
+    last_message = messages[-1]
+    docs = last_message.content
+    # Prompt
+    prompt = hub.pull("rlm/rag-prompt")
+    # LLM
+    llm = create_llm_model()
+    # # Post-processing
+    # def format_docs(docs):
+    #     return "\n\n".join(doc.page_content for doc in docs)
+    # Chain
+    rag_chain = prompt | llm | StrOutputParser()
+    # Run
+    response = rag_chain.invoke({"context": docs, "question": question})
+    return {"messages": [response]}
+
+
+# print("*" * 20 + "Prompt[rlm/rag-prompt]" + "*" * 20)
+# hub.pull("rlm/rag-prompt").pretty_print()  # Show what the prompt looks like
+
+
+# -----------------------------------------------------------------------------
+# Graph
+
+
+def create_graph() -> CompiledStateGraph:
+    """Create a graph for the agent to decide whether to retrieve documents.
+
+    Returns:
+        CompiledStateGraph: A compiled state graph for the agent.
+    """
+    # Define a new graph
+    workflow = StateGraph(AgentState)
+    # Define the nodes we will cycle between
+    workflow.add_node("agent", agent)  # agent
+    retrieve = ToolNode(tools)
+    workflow.add_node("retrieve", retrieve)  # retrieval
+    workflow.add_node("rewrite", rewrite)  # Re-writing the question
+    # Generating a response after we know the documents are relevant
+    workflow.add_node("generate", generate)
+    # Call agent node to decide to retrieve or not
+    workflow.add_edge(START, "agent")
+    # Decide whether to retrieve
+    workflow.add_conditional_edges(
+        "agent",
+        # Assess agent decision
+        tools_condition,
+        {
+            # Translate the condition outputs to nodes in our graph
+            "tools": "retrieve",
+            END: END,
+        },
+    )
+    # Edges taken after the `action` node is called.
+    workflow.add_conditional_edges(
+        "retrieve",
+        # Assess agent decision
+        grade_documents,
+    )
+    workflow.add_edge("generate", END)
+    workflow.add_edge("rewrite", "agent")
+    # Compile
+    return workflow.compile()
+
+
+# -----------------------------------------------------------------------------
+# MAIN
+inputs = {
+    "messages": [
+        ("user", "What does Lilian Weng say about the types of agent memory?"),
+    ]
+}
+graph = create_graph()
+for output in graph.stream(inputs):
+    for key, value in output.items():
+        pprint.pprint(f"Output from node '{key}':")
+        pprint.pprint("---")
+        pprint.pprint(value, indent=2, width=80, depth=None)
+    pprint.pprint("\n---\n")
--- agents/basics.py
+++ agents/basics.py
@@ -5,32 +5,20 @@ Based on: https://langchain-ai.github.io/langgraph/tutorials/introduction/
 """
 
 import json
-from pprint import pprint
-from typing import Annotated, Any, Callable, cast
+from typing import Annotated, Any
 
-import dotenv
 from langchain_community.tools.tavily_search import TavilySearchResults
-from langchain_core.messages import AIMessage, AnyMessage, ToolMessage
-from langchain_core.runnables import RunnableConfig
-from langchain_core.tools import InjectedToolCallId, tool
+from langchain_core.messages import ToolMessage
+from langchain_core.tools import BaseTool
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langgraph.checkpoint.memory import MemorySaver
-from langgraph.graph import END, START, StateGraph  # pylint: disable=W0611
+from langgraph.graph import StateGraph
 from langgraph.graph.message import add_messages
-from langgraph.graph.state import CompiledStateGraph
 from langgraph.prebuilt import ToolNode, tools_condition
-from langgraph.types import Command, interrupt
 from typing_extensions import TypedDict
-
-dotenv.load_dotenv()
 
 
 class BasicToolNode:
-    """
-    A node that runs the tools requested in the last AIMessage.
-
-    Same as: langgraph.prebuilt.toolnode.ToolNode
-    """
+    """A node that runs the tools requested in the last AIMessage."""
 
     def __init__(self, tools_: list) -> None:
         self.tools_by_name = {tool.name: tool for tool in tools_}
@@ -55,229 +43,39 @@ class BasicToolNode:
         return {"messages": outputs}
 
 
-def route_tools(
-    state: list[AIMessage] | dict[str, list[AIMessage]]
-) -> str:  # pylint: disable=W0622
-    """
-    Use in the conditional_edge to route to the ToolNode if the last message
-    has tool calls. Otherwise, route to the end.
-
-    Same as: langgraph.prebuilt.toolnode.tools_condition
-    """
-    if isinstance(state, list):
-        ai_message = state[-1]
-    elif messages := state.get("messages", []):
-        ai_message = messages[-1]
-    else:
-        raise ValueError(
-            f"No messages found in input state to route_tools: {state}"
-        )
-    if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
-        return "tools"
-    return END
+class State(TypedDict):  # pylint: disable=C0115
+    messages: Annotated[list, add_messages]
 
 
-# @tool
-# def human_assistance(query: str) -> str:
-#     """Request assistance from a human."""
-#     human_response = interrupt({"query": query})
-#     return human_response["data"]
+graph_builder = StateGraph(State)
 
 
-@tool
-# Note that because we are generating a ToolMessage for a state update, we
-# generally require the ID of the corresponding tool call. We can use
-# LangChain's InjectedToolCallId to signal that this argument should not
-# be revealed to the model in the tool's schema.
-def human_assistance(
-    name: str,
-    birthday: str,
-    tool_call_id: Annotated[str, InjectedToolCallId],
-) -> str:
-    """Request assistance from a human."""
-    human_response = interrupt(
-        {
-            "question": "Is this correct?",
-            "name": name,
-            "birthday": birthday,
-        },
-    )
-    # If the information is correct, update the state as-is.
-    if human_response.get("correct", "").lower().startswith("y"):
-        verified_name = name
-        verified_birthday = birthday
-        response = "Correct"
-    # Otherwise, receive information from the human reviewer.
-    else:
-        verified_name = human_response.get("name", name)
-        verified_birthday = human_response.get("birthday", birthday)
-        response = f"Made a correction: {human_response}"
-
-    # This time we explicitly update the state with a ToolMessage inside
-    # the tool.
-    state_update = {
-        "name": verified_name,
-        "birthday": verified_birthday,
-        "messages": [ToolMessage(response, tool_call_id=tool_call_id)],
-    }
-    # We return a Command object in the tool to update our state.
-    return Command(update=state_update)  # type: ignore[return-value]
+tool: BaseTool = TavilySearchResults(max_results=2)
+tools = [tool]
+llm = ChatGoogleGenerativeAI(
+    model="gemini-1.5-pro",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+)
+llm_with_tools = llm.bind_tools(tools)  # type: ignore
 
 
-class GeminiAgent:
-    """Simple gemini agent that uses a chatbot and tools."""
-
-    class State(TypedDict):
-        messages: Annotated[list[AnyMessage], add_messages]
-        name: str
-        birthday: str
-
-    def __init__(self):
-        self.tools = [TavilySearchResults(max_results=2), human_assistance]
-        self.llm = ChatGoogleGenerativeAI(
-            model="gemini-2.0-flash",
-            temperature=0,
-            max_tokens=None,
-            timeout=None,
-            max_retries=2,
-        )
-        self.llm_with_tools = self.llm.bind_tools(self.tools)
-        self.memory = MemorySaver()
-        self.graph = self._build_graph(self.chatbot, self.tools, self.memory)
-        # For MemorySaver
-        self.config = RunnableConfig(configurable={"thread_id": "1"})
-
-    @staticmethod
-    def _build_graph(
-        chatbot: Callable[[State], dict[str, list[AIMessage]]],
-        tools: list[Any],
-        memory: MemorySaver,
-    ) -> CompiledStateGraph:
-        """Build the state graph for the agent.
-
-        Args:
-            chatbot (Callable[[State], dict[str, list[AIMessage]]]):
-                Chatbot function.
-            tools (list[Any]): List of tools.
-            memory (MemorySaver): Memory saver.
-
-        Returns:
-            CompiledStateGraph: The compiled state graph.
-        """
-        graph_builder = StateGraph(GeminiAgent.State)
-        graph_builder.add_node("chatbot", chatbot)
-        graph_builder.add_node("tools", ToolNode(tools))
-        graph_builder.add_conditional_edges("chatbot", tools_condition)
-        # Any time a tool is called, we return to the chatbot to decide
-        # the next step
-        graph_builder.add_edge("tools", "chatbot")
-        # The next 2 lines are identical. The second one is just a shorthand.
-        # graph_builder.add_edge(START, "chatbot")
-        graph_builder.set_entry_point("chatbot")
-        graph = graph_builder.compile(checkpointer=memory)
-        graph.get_graph().draw_mermaid_png(output_file_path="graph.png")
-        return graph
-
-    def chatbot(self, state: State) -> dict[str, list[AIMessage]]:
-        message = cast(AIMessage, self.llm_with_tools.invoke(state["messages"]))
-        # Because we will be interrupting during tool execution,
-        # we disable parallel tool calling to avoid repeating any
-        # tool invocations when we resume.
-        assert len(message.tool_calls) <= 1
-        return {"messages": [message]}
-
-    def get_snapshot(self):
-        snapshot = self.graph.get_state(self.config)
-        print("=" * 80)
-        print("Snapshot: ", snapshot)
-        print("Next Snapshot: ", snapshot.next)
-
-    def stream(self, input: str):  # pylint: disable=W0622
-        """Stream the input through the graph and print the output.
-
-        Args:
-            input (str): The input to the agent.
-        """
-        # for event in self.graph.stream(
-        #     {"messages": [{"role": "user", "content": input}]},
-        #     config=self.config,
-        # ):
-        #     for value in event.values():
-        #         print("Assistant:", value["messages"][-1].content)
-
-        events = self.graph.stream(
-            {"messages": [{"role": "user", "content": input}]},
-            config=self.config,
-            stream_mode="values",
-        )
-        for event in events:
-            event["messages"][-1].pretty_print()
-
-        # # For `human_assistance(query: str)` tool
-        # if self.graph.get_state(self.config).tasks[0].interrupts != ():
-        #     human_response = (
-        #         "We, the experts are here to help! We'd recommend you "
-        #         "check out LangGraph to build your agent."
-        #         " It's much more reliable and extensible than simple "
-        #         "autonomous agents."
-        #     )
-        #     human_command: Command = Command(resume={"data": human_response})
-        #     resumed_events = self.graph.stream(
-        #         human_command,
-        #         config=self.config,
-        #         stream_mode="values",
-        #     )
-        #     for resumed_event in resumed_events:
-        #         resumed_event["messages"][-1].pretty_print()
-
-        if self.graph.get_state(self.config).tasks[0].interrupts != ():
-            human_command: Command = Command(
-                resume={
-                    "name": "LangGraph",
-                    "birthday": "Jan 17, 2024",
-                },
-            )
-            resumed_events = self.graph.stream(
-                human_command,
-                config=self.config,
-                stream_mode="values",
-            )
-            for resumed_event in resumed_events:
-                resumed_event["messages"][-1].pretty_print()
-
-        # To get the state.
-        snapshot = self.graph.get_state(self.config)
-        pprint(snapshot.values)
-
-        # To update the state.
-        # self.graph.update_state(self.config, {"name": "LangGraph (library)"})
+def chatbot(state: State):  # pylint: disable=C0116
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
 
 
-if __name__ == "__main__":
+graph_builder.add_node("chatbot", chatbot)
 
-    agent = GeminiAgent()
+tool_node = ToolNode(tools=[tool])
+graph_builder.add_node("tools", tool_node)
 
-    # while True:
-    #     try:
-    #         user_input = input("User: ")
-    #         if user_input.lower() in ["quit", "exit", "q"]:
-    #             print("Goodbye!")
-    #             break
-
-    #         agent.stream(user_input)
-    #     except Exception as e:  # pylint: disable=W0702
-    #         # fallback if input() is not available
-    #         user_input = "What do you know about LangGraph?"
-    #         print("User: " + user_input)
-    #         agent.stream(user_input)
-    #         break
-
-    # user_input = (
-    #     "I need some expert guidance for building an AI agent. "
-    #     "Could you request assistance for me?"
-    # )
-    user_input = (
-        "Can you look up when LangGraph was released? "
-        "When you have the answer, use the human_assistance tool for review."
-    )
-    agent.stream(user_input)
+graph_builder.add_conditional_edges(
+    "chatbot",
+    tools_condition,
+)
+# Any time a tool is called, we return to the chatbot to decide the next step
+graph_builder.add_edge("tools", "chatbot")
+graph_builder.set_entry_point("chatbot")
+graph = graph_builder.compile()
